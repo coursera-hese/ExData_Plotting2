fa749cf55b085cf59ecf5347419978220a0c810c
Helgar Seyringer <Helgar.Seyringer@v-research.at>
2019-02-01 10:13:43 +0100

    update

---
+++ plot5.R
@@ -0,0 +1,19 @@
+#
+# plot 5
+#
+download.file("https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip", destfile = "NEI_data.zip")
+unzip("NEI_data.zip")
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+
+# Prepare Data
+#library(dplyr)
+
+
+# Plotting
+#library(ggplot2)
+png(filename = "plot5.png")
+
+
+dev.off()
