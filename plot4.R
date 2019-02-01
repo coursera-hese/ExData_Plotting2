4e5b5c1bf98d816d5cdb494986ff25980554b2de
Helgar Seyringer <Helgar.Seyringer@v-research.at>
2019-02-01 10:19:55 +0100

    update

--- plot4.R
+++ plot4.R
@@ -26,27 +26,23 @@ pm0 <- df %>% filter(year == 1999)
 pm1 <- df %>% filter(year == 2008)
 sum0 <- with(pm0, tapply(Emissions, SCC, sum, na.rm = TRUE))
 sum1 <- with(pm1, tapply(Emissions, SCC, sum, na.rm = TRUE))
-
-summary(sum0)
-summary(sum1)
+#summary(sum0)
+#summary(sum1)
 
 d0 <- data.frame(scc = names(sum0), sum = sum0)
 d1 <- data.frame(scc = names(sum1), sum = sum1)
-
-head(d0)
-head(d1)
+#head(d0)
+#head(d1)
 
 mrg <- merge(d0, d1, by = "scc")
-# 56  3
-dim(mrg)
-
-head(mrg)
+## dim: 56  3
+#dim(mrg)
+#head(mrg)
 
 # Plotting
 png(filename = "plot4.png")
 
 par(mfrow = c(1, 1))
-#with(mrg, plot(rep(1999, 56),  mrg[, 2], xlim = c(1999, 2008), log = "y"))
 with(mrg, 
      plot(rep(1999, 56),  mrg[, 2], xlim = c(1999, 2008), log = "y", 
      main = "Coal-Combustion Emissions (USA, 1999 vs. 2008)",
@@ -57,21 +53,17 @@ segments(rep(1999, 56), mrg[,2], rep(2008, 56), mrg[,3])
 dev.off()
 
 
-# ALTERNATIVE: Plotting
-library(ggplot2)
-png(filename = "plot4.png")
-
-ggplot(df, aes(year, Emissions)) +
-  labs(title = "Coal-Combustion Emissions by Year (USA, 1999 to 2008)",
-       x = "Year", y = "PM2.5 Emissions [tons]") +
-  geom_point(color = "black", size = 5, alpha = 0.6) +
-  #geom_point(aes(color = Emissions), size = 15, alpha = 1/2) + 
-  #scale_y_log10() +
-  coord_cartesian(ylim = c(0, 200)) +  # include outliers (see 'qqplot2'-PDF, "Axis Limits")
-  #geom_line(y = 0.0, color = "red") +
-  geom_smooth(method = "lm", size = 2) + 
-  theme_bw()
-
-dev.off()
+## ALTERNATIVE: Plotting
+#library(ggplot2)
+#png(filename = "plot4.png")
+#
+#ggplot(df, aes(year, Emissions)) +
+#  labs(title = "Coal-Combustion Emissions by Year (USA, 1999 to 2008)", x = "Year", y = "PM2.5 Emissions [tons]") +
+#  geom_point(color = "black", size = 5, alpha = 0.6) +
+#  coord_cartesian(ylim = c(0, 200)) +  # include outliers (see 'qqplot2'-PDF, "Axis Limits")
+#  geom_smooth(method = "lm", size = 2) + 
+#  theme_bw()
+#
+#dev.off()
 
 # The smooth-line of the PM2.5 emissions has decreased for all types.
